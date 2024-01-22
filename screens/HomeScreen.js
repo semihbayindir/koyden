b1dfaff05775696b00a0c21230c83426a8d8056c
bbsulun <bihterberresulun5@gmail.com>
2024-01-22 23:38:39 +0300

    deneme

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,34 +1,38 @@
-import { View, Text, StyleSheet, TouchableOpacity, Image, Pressable } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, Pressable, ScrollView } from 'react-native';
 import React from 'react';
+
 
 const HomeScreen = ({ navigation }) => {
   return (
+
+    <ScrollView>
     <View style={{backgroundColor:'#f9fbe5'}}>
     <View style={styles.container}>
       <Pressable style={styles.cardWhite}
       onPress={() => navigation.navigate("Uretici")}
       >
         <Text style={styles.roleTitle}>ÜRETİCİ</Text>
-        <Image style={styles.roleImage} source={require("/Users/semihbayindir/koyden/assets/home/large-set-isolated-vegetables-white-background.png")} />
+        <Image style={styles.roleImage} source={require("../assets/home/large-set-isolated-vegetables-white-background.png")} />
       </Pressable>
 
       <TouchableOpacity style={styles.cardGreen}>
         <Text style={styles.roleTitle}>TÜKETİCİ</Text>
-        <Image style={styles.roleImage}  source={require("/Users/semihbayindir/koyden/assets/home/174209-basket-fresh-fruit-free-photo.png")} />
+        <Image style={styles.roleImage}  source={require("../assets/home/174209-basket-fresh-fruit-free-photo.png")} />
       </TouchableOpacity>
 
       <TouchableOpacity style={styles.cardGray}>
         <Text style={styles.roleTitle}>TAŞIYICI</Text>
-        <Image style={styles.roleImage} source={require("/Users/semihbayindir/koyden/assets/home/Trucking-Background-PNG.png")} />
+        <Image style={styles.roleImage} source={require("../assets/home/Trucking-Background-PNG.png")} />
       </TouchableOpacity>
     </View>
     </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 20,
+    marginTop: 15,
   },
   roleTitle: {
     paddingLeft:20,
@@ -38,7 +42,7 @@ const styles = StyleSheet.create({
   },
   cardWhite: {
     backgroundColor: '#fff',
-    marginBottom: 20,
+    marginBottom: 15,
     marginLeft: '5%',
     width: '90%',
     shadowColor: '#000',
@@ -48,7 +52,7 @@ const styles = StyleSheet.create({
   },
   cardGreen: {
     backgroundColor: '#cde8b5',
-    marginBottom: 20,
+    marginBottom: 15,
     marginLeft: '5%',
     width: '90%',
     shadowColor: '#000',
@@ -58,7 +62,7 @@ const styles = StyleSheet.create({
   },
   cardGray: {
     backgroundColor: '#d6d7cf',
-    marginBottom: 10,
+    marginBottom: 15,
     marginLeft: '5%',
     width: '90%',
     shadowColor: '#000',
@@ -68,7 +72,7 @@ const styles = StyleSheet.create({
   },
   roleImage: {
     width: '100%',
-    height: 200,
+    height: 180,
     resizeMode: 'cover',
     overflow: 'visible'
   },
