0c143569aedf8afc8b4fb76feaba5c628c8b15b9
bbsulun <bihterberresulun5@gmail.com>
2024-05-08 16:02:03 +0300

    ürün sayfaları design güncellenmesi

--- components/AllProducts.js
+++ components/AllProducts.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import { useUserIdDecoder } from './UserIdDecoder';
 import SearchBar from './SearchBar';
 import { fuzzySearch } from './FuzzySearch';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
 
@@ -37,6 +38,29 @@ const AllProducts = () => {
     navigation.navigate('SingleProduct', { productId: productId });
   };
 
+
+
+
+  const handleAddToCart = async (productId) => {
+    try {
+      const response = await axios.post(`http://localhost:8000/cart/add/${userId}`, {
+        productId,
+        quantity: 1 // Üründen sadece bir adet sepete eklenecek
+      });
+      console.log('Product added to cart:', response.data);
+      Alert.alert('Ürün sepete eklendi.');
+    } catch (error) {
+      console.log(userId)
+      console.error('Error adding product to cart:', error);
+    }
+  };
+  
+
+
+
+
+
+
   const [producerInfos, setProducerInfos] = useState([]);
 
 useEffect(() => {
@@ -56,21 +80,33 @@ useEffect(() => {
   fetchProducerInfo();
 }, [products]);
 
+
 const renderProductItem = ({ item, index }) => {
   const producerInfo = producerInfos[index];
   return (
     <TouchableOpacity style={styles.urunler} onPress={() => handleProductPress(item._id)}>
       {item.images && item.images.length > 0 ? (
-        <Image style={styles.images} source={{ uri: item.images[0] }} /> 
+        <View>
+          <Image style={styles.images} source={{ uri: item.images[0] }} /> 
+          
+        </View>
       ) : (
         <View />
       )}
-      <View style={styles.productInfo}>
-        <Text style={styles.productName}>{item.name}</Text>
+     
+      <View style={styles.prductInfo}>
+
+        <View style={{flexDirection:'row', flex:1}}>
+            <Text style={styles.productName}>{item.name}</Text>
+            <TouchableOpacity style={{alignItems:'flex-end',marginRight:4, flex:0.3}} onPress={() => handleAddToCart(item._id)}>
+            <MaterialCommunityIcons name='plus-circle' size={40} color={'#de510b'}/>
+          </TouchableOpacity>
+        </View>
+        
         {/* <Text style={styles.producerAddress}>{producerInfo && producerInfo.verification.producerAddress.city}</Text> */}
         <View style={styles.productDet}>
           <Text style={styles.productFrom}>{producerInfo && producerInfo.verification.producerAddress.city}</Text>
-          <Text style={styles.productPrice}>{item.price} ₺</Text>
+          <Text style={styles.productPrice}>{item.price}₺</Text>
         </View>
       </View>
     </TouchableOpacity>
@@ -88,7 +124,7 @@ const renderProductItem = ({ item, index }) => {
             value={searchKeyword}
             onChangeText={setSearchKeyword}
           />
-          <View style={{flexDirection:'row', alignItems:'center', marginTop:5, marginBottom:10 }}>
+          <View style={{flexDirection:'row', alignItems:'center', marginTop:5, marginBottom:10}}>
             <TouchableOpacity style={styles.butons}>
               <Text style={{fontSize:18}}>Tümü</Text>
             </TouchableOpacity>
@@ -137,11 +173,9 @@ const renderProductItem = ({ item, index }) => {
       padding:4
     },
     urunler:{
-      backgroundColor:'#d8e3c3',
+      backgroundColor:'#fff',
       marginHorizontal:10,
-      borderWidth:2,
-      borderRadius:10,
-      borderColor:'#9ab863',
+      borderRadius:20,
       width: '45%',
       justifyContent:'center',
       marginBottom:10,
@@ -149,9 +183,8 @@ const renderProductItem = ({ item, index }) => {
     images: {
       width: '100%',
       height: 140,
-      marginTop:10,
-      resizeMode: 'contain',
-      
+      marginBottom:10,
+      borderRadius:20
     },
     input: {
       borderWidth:1,
@@ -170,20 +203,24 @@ const renderProductItem = ({ item, index }) => {
     productName:{
       fontSize:18, 
       padding:5,
+      flex:0.7
     },
     productFrom:{
       fontSize:18, 
       paddingLeft:5, 
       paddingBottom:10,
-      color:'green'
+      color:'green',
+      flex:0.7
     },
     productPrice:{
       fontSize:18, 
-      paddingLeft:30, 
+      flex:0.3,
       paddingBottom:10,
+
     },
     productDet:{
       flexDirection:'row',
+      flex:1
     },
   
   
